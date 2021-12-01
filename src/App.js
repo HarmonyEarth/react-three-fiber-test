6d86fdfb6aadc82ec3421cf2168c4722e6885bab
HarmonyEarth <sentai@chowa.dev>
2021-12-01 15:23:07 -0500

    Initial Commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,50 @@
-import logo from './logo.svg';
 import './App.css';
+import { Canvas } from '@react-three/fiber';
+import { Html, OrbitControls, SpotLight, Stars } from '@react-three/drei';
+import { Physics, useBox, usePlane } from '@react-three/cannon';
+
+
+const Box = () => {
+  const [boxRef, api] = useBox(() => ({mass:1}));
+  return(
+    <mesh onClick={() => {
+      api.velocity.set(Math.random(),4,Math.random())
+    }} ref={boxRef} position={[0, 5, 0]}>
+      <boxBufferGeometry attach="geometry" />
+      <meshLambertMaterial attach="material" color="pink"/>
+    </mesh>
+  );
+}
+
+const Plane = () => {
+  const [planeRef] = usePlane(() => ({ rotation: [-Math.PI / 2,0,0], }));
+  return (
+    <mesh position={[0, 0, 0]} rotation={[-Math.PI / 2,0,0]} >
+      <planeBufferGeometry attach='geometry' args={[100,100]}/>
+      <meshLambertMaterial attach='material' color='gold' />
+    </mesh>
+  );
+}
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <Canvas>
+        <OrbitControls />
+        <Stars />
+        <ambientLight intensity={0.5} />
+        <SpotLight position={[-1,4,1]} angle={0.4} />
+        <SpotLight position={[-3,2,-2]} angle={0.6} />
+       <Html
+          as='div' // Wrapping element (default: 'div')
+          center // Adds a -50%/-50% css transform (default: false) [ignored in transform mode]
+          >
+            <h4 style={{color:'blue' }}>Box</h4>
+        </Html> 
+        <Physics>
+        <Box />
+        <Plane />
+        </Physics>
+      </Canvas>
   );
 }
 
